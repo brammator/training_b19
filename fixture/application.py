6c5f934e56e7938d427b12848743d338b2c7a7c9
Ilya Marchev <marchev-i@bercut.com>
2020-12-04 04:51:24 +0300

    Опции командной строки

--- fixture/application.py
+++ fixture/application.py
@@ -7,9 +7,15 @@ from fixture.session import SessionHelper
 
 
 class Application:
-    def __init__(self):
+    def __init__(self, browser, base_url):
         self.DEFAULT_WAIT_TIME = 5
-        self.wd = webdriver.Firefox()
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
+        self.base_url = base_url
         self.wd.implicitly_wait(self.DEFAULT_WAIT_TIME)
         self.session = SessionHelper(app=self)
         self.group = GroupHelper(app=self)
@@ -22,7 +28,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and
                 len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def is_valid(self):
         try:
