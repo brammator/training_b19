09008cc2693a0ef62cc681f675a37e232474f306
Ilya Marchev <marchev-i@bercut.com>
2020-12-11 01:42:50 +0300

    Чтение настроек из конфигурационного json-файла

--- conftest.py
+++ conftest.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
+import json
+
 from fixture.application import Application
 import pytest
+import os.path
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
+
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--base_url")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as fp:
+            target = json.load(fp)
     if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
+        fixture = Application(browser=browser, base_url=target['base_url'])
     else:
         if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_logged(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=target['base_url'])
+    fixture.session.ensure_logged(username=target["admin"], password=target["secret"])
     return fixture
 
 
@@ -30,7 +39,7 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--base_url", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
 
 
 @pytest.fixture
