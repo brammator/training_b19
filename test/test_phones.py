bdcc72df84fc09802ea75c6e828f17371a4cba24
Ilya Marchev <marchev-i@bercut.com>
2020-12-22 00:47:28 +0300

    исправление проблем с гитом

--- test/test_phones.py
+++ test/test_phones.py
@@ -12,7 +12,7 @@ def test_info_on_home_page(app, x, db):
         app.contact.add(Contact(firstname="John", middlename="Vasilyevitch", lastname="Doe", nickname="Gryazny",
                                 title="Cleanliness Director", company="Dixyorochka", address="SPb",
                                 home="+78005553535", mobile="8-800-555-3535", work="8 (800) 555 3535",
-                             fax="8 800 555 35 35", email="cdur@oldfiction.book",
+                                fax="8 800 555 35 35", email="cdur@oldfiction.book",
                                 homepage="localhost/addressbook", bday="1", bmonth="February", byear="1816",
                                 aday="30", amonth="October", ayear="2020", address2="Msk", phone2="ditto",
                                 notes="What are you, Oleg"))
